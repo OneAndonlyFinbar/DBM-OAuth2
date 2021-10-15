f8fe674a521bd0798eec25e4b23f1613c5e2b961
OneAndonlyFinbar <finbar@fin-bar.xyz>
2021-10-15 19:05:57 -0400

    Add files via upload

---
+++ src/actions/store_user_oauth2.js
@@ -0,0 +1,203 @@
+module.exports = {
+	//---------------------------------------------------------------------
+	// Action Name
+	//
+	// This is the name of the action displayed in the editor.
+	//---------------------------------------------------------------------
+
+	name: "Store Member Oauth2 Info",
+
+	//---------------------------------------------------------------------
+	// Action Section
+	//
+	// This is the section the action will fall into.
+	//---------------------------------------------------------------------
+
+	section: "Member Control",
+
+	//---------------------------------------------------------------------
+	// Action Subtitle
+	//
+	// This function generates the subtitle displayed next to the name.
+	//---------------------------------------------------------------------
+
+	subtitle: function(data) {
+		const members = ["Mentioned User", "Command Author", "Temp Variable", "Server Variable", "Global Variable"];
+		const info = ["Member ID", "Member Email", "Member Guilds"];
+		return `${members[parseInt(data.member)]} - ${info[parseInt(data.info)]}`;
+	},
+
+	//---------------------------------------------------------------------
+	// Action Storage Function
+	//
+	// Stores the relevant variable info for the editor.
+	//---------------------------------------------------------------------
+
+	variableStorage: function(data, varType) {
+		const type = parseInt(data.storage);
+		if(type !== varType) return;
+		const info = parseInt(data.info);
+		let dataType = "Unknown Type";
+		switch(info) {
+			case 0:
+				dataType = "Text";
+				break;
+            case 1:
+                dataType = "Text";
+                break;
+            case 2:
+                dataType = "List";
+                break;
+            default:
+                break;
+		}
+		return ([data.varName2, dataType]);
+	},
+
+	//---------------------------------------------------------------------
+	// Action Fields
+	//
+	// These are the fields for the action. These fields are customized
+	// by creating elements with corresponding IDs in the HTML. These
+	// are also the names of the fields stored in the action's JSON data.
+	//---------------------------------------------------------------------
+
+	fields: ["member", "varName", "info", "storage", "varName2"],
+
+	//---------------------------------------------------------------------
+	// Command HTML
+	//
+	// This function returns a string containing the HTML used for
+	// editting actions.
+	//
+	// The "isEvent" parameter will be true if this action is being used
+	// for an event. Due to their nature, events lack certain information,
+	// so edit the HTML to reflect this.
+	//
+	// The "data" parameter stores constants for select elements to use.
+	// Each is an array: index 0 for commands, index 1 for events.
+	// The names are: sendTargets, members, roles, channels,
+	//                messages, servers, variables
+	//---------------------------------------------------------------------
+
+	html: function(isEvent, data) {
+		return `
+<div>
+	Mod developed by Finbar#0001 (811751283555172433)
+	<a>Documentation</a>
+	<a>Report issue</a>
+	<a>Contribute</a>
+	<div style="float: left; width: 35%;">
+		Source Member:<br>
+		<select id="member" class="round" onchange="glob.memberChange(this, 'varNameContainer')">
+			${data.members[isEvent ? 1 : 0]}
+		</select>
+	</div>
+	<div id="varNameContainer" style="display: none; float: right; width: 60%;">
+		Variable Name:<br>
+		<input id="varName" class="round" type="text" list="variableList"><br>
+	</div>
+</div><br><br><br>
+<div>
+	<div style="padding-top: 8px; width: 70%;">
+		Source Info:<br>
+		<select id="info" class="round">
+			<option value="0" selected>Member ID</option>
+            <option value="1" selected>Member Email</option>
+            <option value="2" selected>Member Guilds</option>
+		</select>
+	</div>
+</div><br>
+<div>
+	<div style="float: left; width: 35%;">
+		Store In:<br>
+		<select id="storage" class="round">
+			${data.variables[1]}
+		</select>
+	</div>
+	<div id="varNameContainer2" style="float: right; width: 60%;">
+		Variable Name:<br>
+		<input id="varName2" class="round" type="text"><br>
+	</div>
+</div>`;
+	},
+
+	//---------------------------------------------------------------------
+	// Action Editor Init Code
+	//
+	// When the HTML is first applied to the action editor, this code
+	// is also run. This helps add modifications or setup reactionary
+	// functions for the DOM elements.
+	//---------------------------------------------------------------------
+
+	init: function() {
+		const { glob, document } = this;
+
+		glob.memberChange(document.getElementById("member"), "varNameContainer");
+	},
+
+	//---------------------------------------------------------------------
+	// Action Bot Function
+	//
+	// This is the function for the action within the Bot's Action class.
+	// Keep in mind event calls won't have access to the "msg" parameter,
+	// so be sure to provide checks for variable existance.
+	//---------------------------------------------------------------------
+
+	action: function(cache) {
+		const data = cache.actions[cache.index];
+		const member = parseInt(data.member);
+		const varName = this.evalMessage(data.varName, cache);
+		const info = parseInt(data.info);
+		const mem = this.getMember(member, varName, cache);
+
+		/*if(!mem) {
+			this.callNextAction(cache);
+			return;
+		}*/
+
+		const MySql = require('sync-mysql');
+		const connection = new MySql({
+			host     : '192.168.1.27',
+			user     : 'DBM-OAUTH2',
+			password : 'Qby?dy#q8@Z-@6EP?8Vu5Mdq^J%LZLNkz=y%52W%Mn%v3TbKP6+8vC@B-8vzSNps'
+		});
+
+		const queryRes = connection.query('SELECT * FROM DBMOAUTH2.data WHERE id = ?', [mem.id]);
+
+		if(!queryRes) return;
+
+		let result;
+		switch(info) {
+			case 0:
+				result = queryRes[0]['id'];
+				break;
+			case 1:
+				result = queryRes[0]['email'];
+				break;
+			case 2:
+				result = queryRes[0]['guilds'];
+				break;
+			default:
+				break;
+		}
+
+		if(result !== undefined) {
+			const storage = parseInt(data.storage);
+			const varName2 = this.evalMessage(data.varName2, cache);
+			this.storeValue(result, storage, varName2, cache);
+		}
+		this.callNextAction(cache);
+	},
+
+	//---------------------------------------------------------------------
+	// Action Bot Mod
+	//
+	// Upon initialization of the bot, this code is run. Using the bot's
+	// DBM namespace, one can add/modify existing functions if necessary.
+	// In order to reduce conflictions between mods, be sure to alias
+	// functions you wish to overwrite.
+	//---------------------------------------------------------------------
+
+	mod: function() {}
+}; // End of module
