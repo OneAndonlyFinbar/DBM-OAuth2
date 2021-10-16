c7b201f92028db316266b5fc3261dd9e3af6f4c5
OneAndonlyFinbar <finbar@fin-bar.xyz>
2021-10-15 20:22:15 -0400

    Update database.inc.php

--- src/php/database.inc.php
+++ src/php/database.inc.php
@@ -1,5 +1,5 @@
 <?php 
-$_DATABASEHOST = '192.168.1.27';
+$_DATABASEHOST = 'localhost';
 $_DATABASEUSER = 'DBM-OAUTH2';
 $_DATABASEPASSWORD = 'Qby?dy#q8@Z-@6EP?8Vu5Mdq^J%LZLNkz=y%52W%Mn%v3TbKP6+8vC@B-8vzSNps';
 
