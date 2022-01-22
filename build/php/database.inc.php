cf0e7a93d6e7b2c5980098178df629d8c3c6fec0
OneAndonlyFinbar <finbar@finbar.xyz>
2022-01-22 13:22:36 -0500

    Delete build directory

--- build/php/database.inc.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$_DATABASEHOST = 'localhost';
-$_DATABASEUSER = 'DBM-OAUTH2';
-$_DATABASEPASSWORD = 'Qby?dy#q8@Z-@6EP?8Vu5Mdq^J%LZLNkz=y%52W%Mn%v3TbKP6+8vC@B-8vzSNps';
-
-$mysqli = mysqli_connect($_DATABASEHOST, $_DATABASEUSER, $_DATABASEPASSWORD, "DBMOAUTH2");
-?>
